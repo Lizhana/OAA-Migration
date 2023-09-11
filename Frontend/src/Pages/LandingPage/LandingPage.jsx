572bcbd55557a6ab38a894e04302b5a9db1fb369
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-09-11 02:18:10 -0500

    merge develop with gallery

--- Frontend/src/Pages/LandingPage/LandingPage.jsx
+++ Frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -6,20 +6,15 @@ import Communities from "../../Components/LandingPage/Communities/Communities";
 import Agroecology from "../../Components/LandingPage/Agroecology/Agroecology";
 import MostReadNews from "../../Components/LandingPage/MostReadNews/MostReadNews";
 import Footer from "../../Components/LandingPage/Footer/Footer";
-<<<<<<< HEAD:Frontend/src/Pages/LadingPage/LandingPage.jsx
 import styles from './LandingPage.module.css';
 import Gallery from '../Gallery/Gallery';
-=======
 import Newsletter from '../../Components/LandingPage/Newsletter/Newsletter';
->>>>>>> 0e5efc63402fea2c844b9789151177c595cbec24:Frontend/src/Pages/LandingPage/LandingPage.jsx
 
-export default function LandingPage() {
-
-<<<<<<< HEAD:Frontend/src/Pages/LadingPage/LandingPage.jsx
 export default function LandingPage () {
     
     return (
         <div>
+            <Newsletter />
             <Hero />
             <PresentationIcons />
             <Novelty />
@@ -30,20 +25,4 @@ export default function LandingPage () {
             <Footer />
         </div>
     );
-};
-=======
-  return (
-    <>
-      <Newsletter />
-      <Hero />
-      <PresentationIcons />
-      <Novelty />
-      <Communities />
-      <Agroecology />
-      <MostReadNews />
-      <Galery />
-      <Footer />
-    </>
-  );
-};
->>>>>>> 0e5efc63402fea2c844b9789151177c595cbec24:Frontend/src/Pages/LandingPage/LandingPage.jsx
+}
