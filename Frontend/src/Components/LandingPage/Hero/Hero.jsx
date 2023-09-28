bae92b682398168eb95354146a94f12a75460d7b
mpvilamarin <mpvillamarin@gmail.com>
2023-09-28 19:34:03 -0300

    ajustes

--- Frontend/src/Components/LandingPage/Hero/Hero.jsx
+++ Frontend/src/Components/LandingPage/Hero/Hero.jsx
@@ -10,25 +10,27 @@ export default function Carousel() {
 
   return (
     <div className={styles.carousel}>
+      <div className={styles.contCarousel}>
+        <Slider
+          autoplay={true}
+          autoplaySpeed={4000}
+          initialSlide={2}
+          infinite={true}
+          dotsClass="slick-dots custom-indicator"
+        >
+          {data.map((item) => (
+            <img key={item} src={item} alt="Imágenes"/>
+          ))}
+        </Slider>
+      </div>
       <div className={styles.landing}>
-        <p className={styles.landingParagraph}>Somos una organización sin fines de lucro que tiene por objeto asesorías ambientales ante problemáticas de contaminación en contextos sociales.</p>
+        <p className={styles.landingParagraph}>Junto a las comunidades en conflicto, por el derecho humano a un ambiente sano.</p>
         <button className={styles.buttonWe}>
           <Link to="/nosotros">
             Sobre Nosotros
           </Link>
         </button >
       </div>
-      <Slider
-        autoplay={true}
-        autoplaySpeed={4000}
-        initialSlide={2}
-        infinite={true}
-        dotsClass="slick-dots custom-indicator"
-      >
-        {data.map((item) => (
-          <img key={item} src={item} alt="Imágenes" style={{ width: "100%" }} />
-        ))}
-      </Slider>
     </div>
   );
 };
--- Frontend/src/Components/LandingPage/Hero/data.js
+++ Frontend/src/Components/LandingPage/Hero/data.js
@@ -1,8 +1,9 @@
-import image1 from "../../../assets/Fondo1.jpg";
-import image2 from "../../../assets/Fondo2.jpg";
-import image3 from "../../../assets/Fondo3.jpg";
-import image4 from "../../../assets/Fondo4.jpg";
-import image5 from "../../../assets/Fondo5.jpg";
+import image1 from "../../../assets/Hero/imagen1.jpg";
+import image2 from "../../../assets/Hero/imagen2.jpg";
+import image3 from "../../../assets/Hero/imagen3.jpg";
+import image4 from "../../../assets/Hero/imagen4.jpg";
+import image5 from "../../../assets/Hero/imagen5.jpg";
+import image6 from "../../../assets/Hero/imagen6.jpg";
+import image7 from "../../../assets/Hero/imagen7.jpg";
 
-
-export const data = [image1, image2, image3, image4, image5];
+export const data = [image1, image2, image3, image4, image5, image6, image7];
